d28694e47e68e557540418638facc3669768674b
Daniel Dixon <code@danieldixon.com>
2014-12-15 00:04:21 -0600

    Updated addLineItem to account for discounted amounts.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
-    
+
   # override in your app if there are promotions you wish to exclude
   def send_to_impact_radius?
     self.completed? && self.all_adjustments.promotion.eligible.any?
