cb6f3144abf40ddefba92e74e1327f3890a48fba
Daniel Dixon <code@danieldixon.com>
2015-02-18 14:58:23 -0600

    Simplified requirements for sending order to IR.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,7 +2,7 @@ Spree::Order.class_eval do
 
   # override in your app if there are promotions you wish to exclude
   def send_to_impact_radius?
-    self.completed? && self.all_adjustments.promotion.eligible.any?
+    self.completed? # && self.all_adjustments.promotion.eligible.any?
   end
 
 end
