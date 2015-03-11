fdbfeeaa3f131ee491425d616e17e555815abe8b
Daniel Dixon <code@danieldixon.com>
2015-03-11 17:43:51 -0500

    Another revision to line item discounted amount calculation.

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,17 +1,18 @@
 Spree::LineItem.class_eval do
 
   def discounted_amount_minus_discounted_amount_from_order
-    self.discounted_amount - self.discounted_amount_from_order
+    discounted_amount - discounted_amount_from_order
   end
 
   private
 
     def discounted_amount_from_order
-      self.order.adjustments.promotion.eligible.sum(:amount).abs * self.percentage_of_order
+      # DD: order.promo_total includes promo adjustments on line items, shipping, and order adjustments
+      (order.promo_total.abs - discounted_amount) * percentage_of_order
     end
 
     def percentage_of_order
-      self.amount / self.order.item_total
+      amount / order.item_total
     end
 
 end
